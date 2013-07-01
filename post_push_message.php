5c312836d9ccfe77433e0e69387560e829d45473
yavuzsel <yavuzsel@buffalo.edu>
2013-07-01 12:17:49 -0400

    push support revisited and push ui is prepared

--- post_push_message.php
+++ post_push_message.php
@@ -9,6 +9,7 @@
     if(isset($_REQUEST['location']) && strlen($_REQUEST['location']) > 0) {
         // location is received. send message to everyone in that location
         $location = $_REQUEST['location'];
+	$location = ldap_escape($location);
         
         // get uids given location
         $ds=ldap_connect($ldap_url);
@@ -27,10 +28,15 @@
         // send messages to uids
         if(count($uids_array) > 0){
             // request only if there are employees to push the message
-            $uids_array = array("dkhushra");
             $data = array("message" => array("alert" => $message, "badge" => 1), "alias" => $uids_array);
-            push_data_to_url($data, $agpush_selected_url, "Selected");
-        }
+            push_data_to_url($data, $agpush_selected_url, "Location-based");
+        } else {
+	    // set the status
+	    header('HTTP/1.1 200 OK');
+	    // set the content type
+	    header('content-type: application/json');
+	    echo json_encode(array("response" => "Location based message, no receivers found!"));
+	}
         
     } else {
         // no location is received. send to everbody? or do nothing?
@@ -57,7 +63,55 @@
         header('HTTP/1.1 200 OK');
         // set the content type
         header('content-type: application/json');
-        echo json_encode($type_message. " " . $result);
+        echo json_encode(array("response" => $type_message. " " . $result));
         curl_close ($ch);
+    }
+    
+    // need to escape special chars in location. so following function helps here.
+    
+    /**
+    * function ldap_escape
+    * @author Chris Wright
+    * @version 2.0
+    * @param string $subject The subject string
+    * @param bool $dn Treat subject as a DN if TRUE
+    * @param string|array $ignore Set of characters to leave untouched
+    * @return string The escaped string
+    */
+    function ldap_escape ($subject, $dn = FALSE, $ignore = NULL) {
+    
+	// The base array of characters to escape
+	// Flip to keys for easy use of unset()
+	$search = array_flip($dn ? array('\\', ',', '=', '+', '<', '>', ';', '"', '#') : array('\\', '*', '(', ')', "\x00"));
+    
+	// Process characters to ignore
+	if (is_array($ignore)) {
+	    $ignore = array_values($ignore);
+	}
+	for ($char = 0; isset($ignore[$char]); $char++) {
+	    unset($search[$ignore[$char]]);
+	}
+    
+	// Flip $search back to values and build $replace array
+	$search = array_keys($search); 
+	$replace = array();
+	foreach ($search as $char) {
+	    $replace[] = sprintf('\\%02x', ord($char));
+	}
+    
+	// Do the main replacement
+	$result = str_replace($search, $replace, $subject);
+    
+	// Encode leading/trailing spaces in DN values
+	if ($dn) {
+	    if ($result[0] == ' ') {
+		$result = '\\20'.substr($result, 1);
+	    }
+	    if ($result[strlen($result) - 1] == ' ') {
+		$result = substr($result, 0, -1).'\\20';
+	    }
+	}
+    
+	return $result;
     }
 ?>
