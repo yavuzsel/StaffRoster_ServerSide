7b12355b3284301e1fc2ee7293a3581bf9c1f905
yavuzsel <yavuzsel@buffalo.edu>
2013-07-08 15:31:36 -0400

    add constants.php

---
+++ constants.php
@@ -0,0 +1,23 @@
+<?php
+	$ldap_url = "URL_TO_LDAP";
+	$ldap_dn = "LDAP_DN";
+	$ldap_query_head = "MAIN_Q1H";
+	$ldap_query_tail = "MAIN_Q1T";
+	$ldap_mngr_query_head = "MAN_QH";
+	$ldap_mngr_filter_query_head = "MANF_QH";
+	$ldap_modifytime_query_head = "MODT_QH";
+	$ldap_sync_modifytime_query_head = "SMODT_QH";
+	$alternate_time_query = "ALT_TQ";
+	$usr = "DOCSPACE_USERNAME";
+	$pwd = "DOSCPACE_PASS";
+	$base_url = "http://localhost";
+	$docspace_users_endpoint = "DOCSPACE_USERS_API";
+	$docspace_images_endpoint = "DOCSPACE_IMAGES_API";
+	$img_dir = "IMG_DIR";
+	$location_query_head = "LOC_QH";
+	$location_query_tail = "LOC_QT";
+	$agpush_broadcast_url = "http://localhost:8080/ag-push/rest/sender/broadcast";
+	$msr_pushapplicationid = "PUSH_APP_ID";
+	$msr_mastersecret = "PUSH_APP_SECRET";
+	$agpush_selected_url = "http://localhost:8080/ag-push/rest/sender/selected";
+?>
